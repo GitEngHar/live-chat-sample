6004a73ccd3a269ba583a00e45d9391afe1acdd4
GitEngHar <harukii1999@gmail.com>
2026-09-09 12:11:48 +0900

    add route

--- message-api/config/routes.rb
+++ message-api/config/routes.rb
@@ -3,10 +3,20 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  #
   get "up" => "rails/health#show", as: :rails_health_check
-  namespace :chat do
-      post "streams/create"
+
+  namespace :user do # 簡単なユーザー管理基盤
+    post "create", to: "user#create" # ユーザー登録
+    get "get", to: "user#get" # ユーザー取得
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+  namespace :room do
+    post "create", to: "room#create" # メッセージチャットの投稿
+    put "action", to: "room#action" # ユーザーのルーム参加状態管理
+  end
+
+  namespace :chat do
+      post "streams/create", to: "streams#create" # メッセージチャットの投稿
+  end
 end
